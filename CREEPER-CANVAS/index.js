64f4326e7d6f36a063b8d77a475450bb8a25178d
Reaper Diper <105932267+Yormenxx@users.noreply.github.com>
2023-01-19 14:03:24 -0500

    Add files via upload

---
+++ CREEPER-CANVAS/index.js
@@ -0,0 +1,24 @@
+
+let $pantalla = document.querySelector("canvas")
+
+let $pencil = $pantalla.getContext("2d")
+
+$pencil.fillStyle="green"
+$pencil.fillRect(0,0,350,300)
+
+
+
+$pencil.fillStyle="black"
+$pencil.fillRect(30,40,90,90)
+
+$pencil.fillStyle="black"
+$pencil.fillRect(230,40,90,90)
+
+$pencil.fillStyle="black"
+$pencil.fillRect(114,130,120,120)
+
+$pencil.fillStyle="black"
+$pencil.fillRect(50,200,65,100)
+
+$pencil.fillStyle="black"
+$pencil.fillRect(234,200,65,100)
